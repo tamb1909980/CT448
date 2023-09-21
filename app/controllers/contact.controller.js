15b62e7af8f7b28cec1d9618dc533a97b1b0b6c2
tamb1909980 <113996680+tamb1909980@users.noreply.github.com>
2023-09-21 16:15:43 +0700

    Create contact.controller.js

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+export.create = (req, res) =>{
+    res.send({ message: "create handler"});
+};
+
+export.findAll = (req, res) => {
+    res.send({ message: "findAll handler"});
+};
+
+export.findOne = (req, res) => {
+    res.send({ message: "findOne handler"});
+};
+
+export.update = (req, res) => {
+    res.send({ message: "update handler"});
+};
+
+export.delete = (req, res) => {
+    res.send({ message: "delete handler"});
+};
+
+export.deleteAll = (req, res) => {
+    res.send({ message: "deleteAll handler"});
+};
+
+export.findAllFavorite = (req, res) => {
+    res.send({ message: "findAllFavorite handler"});
+};
