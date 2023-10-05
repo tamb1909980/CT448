b33421fc54f659b0f893b530ac2a7f415e70710c
tamb1909980 <113996680+tamb1909980@users.noreply.github.com>
2023-10-05 14:44:43 +0700

    Update contact.route.js

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -10,10 +10,10 @@ router.route("/")
 
 router.route("/favorite")
     .get(contacts.findAllFavorite);
- 
+
 router.route("/:id")
     .get(contacts.findOne)
-    .get(contacts.update)
-    .get(contacts.delete);
-
-module.exports = router;
+    .put(contacts.update)
+    .delete(contacts.delete);
+    
+module.exports = router;    
